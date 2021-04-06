5f4859d0eb76bdab9acc869df506495ade9f31d8
syyCN <sunyiyangcn@outlook.com>
2021-04-06 22:42:14 +0800

    add menu to ssider

--- frontend/src/components/global/ssider/index.js
+++ frontend/src/components/global/ssider/index.js
@@ -8,6 +8,7 @@ import {
   FileSyncOutlined,
 } from '@ant-design/icons';
 import './style.scss';
+import { BrowserRouter as Link } from 'react-router-dom';
 
 const { SubMenu } = Menu;
 
@@ -15,13 +16,23 @@ function Ssider() {
   return (
     <div className="s-sider">
       <Menu mode="inline" theme="dark">
-        <Menu.Item icon={<AppstoreAddOutlined />}>添加设备</Menu.Item>
+        <Menu.Item icon={<AppstoreAddOutlined />}>
+          <Link to="/add">添加设备</Link>
+        </Menu.Item>
         <SubMenu title="设备状态管理" icon={<AppstoreOutlined />}>
-          <Menu.Item icon={<FileSearchOutlined />}>搜索设备</Menu.Item>
-          <Menu.Item icon={<FileTextOutlined />}>设备列表</Menu.Item>
-          <Menu.Item icon={<FileSyncOutlined />}>设备状态变更记录</Menu.Item>
+          <Menu.Item icon={<FileSearchOutlined />}>
+            <Link to="/search">搜索设备</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FileTextOutlined />}>
+            <Link to="/list">设备列表</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FileSyncOutlined />}>
+            <Link to="/status">设备状态变更记录</Link>
+          </Menu.Item>
         </SubMenu>
-        <Menu.Item icon={<AlertOutlined />}>设备报警记录</Menu.Item>
+        <Menu.Item icon={<AlertOutlined />}>
+          <Link to="/alert">设备报警记录</Link>
+        </Menu.Item>
       </Menu>
     </div>
   );
