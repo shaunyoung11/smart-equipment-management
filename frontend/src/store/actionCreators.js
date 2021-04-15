7ca34fcc74bc3513190547d0d7fd6363ef73b10f
syyCN <sunyiyangcn@outlook.com>
2021-04-15 13:34:37 +0800

    add to actionCreators

--- frontend/src/store/actionCreators.js
+++ frontend/src/store/actionCreators.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import url from '../config';
-import { GET_DEVICE_LIST, GET_DEVICE_BY_NAME } from './actionTypes';
+import {
+  GET_DEVICE_LIST,
+  GET_DEVICE_BY_NAME,
+  GET_DEVICE_CARRY_RECORD,
+  GET_DEVICE_CIRCULATE_RECORD,
+} from './actionTypes';
 import { notification } from 'antd';
 
 // 设置后端根路径
@@ -46,19 +51,70 @@ export const getDeviceList = () => {
 /**
  * 创建 redux 需要的 action
  * @param {Array} value
- * @returns 返回中间件可用的 action
+ * @returns 返回 Redux 可用的 action
  */
 const getDeviceByNameAction = (value) => ({
   type: GET_DEVICE_BY_NAME,
   value,
 });
 
+/**
+ * 通过设备名称搜索匹配的设备
+ * @param {String} name
+ * @returns 返回中间件可用的 action
+ */
 export const getDeviceByName = (name) => {
   return (dispatch) => {
     axios.get('/device/search?keyword=' + name).then((res) => {
       console.log(res);
       const action = getDeviceByNameAction(res.data.data.deviceList);
       dispatch(action);
+    });
+  };
+};
+
+/**
+ * 创建 redux 需要的 action
+ * @param {Array} value
+ * @returns 返回 Redux 可用的 action
+ */
+const getDeviceCarryRecordAction = (value) => ({
+  type: GET_DEVICE_CARRY_RECORD,
+  value,
+});
+
+/**
+ * 通过设备 ID，获取设备的带出记录
+ * @param {String} id
+ * @returns 返回中间件可用的 action
+ */
+export const getDeviceCarryRecord = (id) => {
+  return (dispatch) => {
+    axios.get('/warnRecord?deviceId=' + id).then((res) => {
+      console.log(res);
+    });
+  };
+};
+
+/**
+ * 创建 redux 需要的 action
+ * @param {Array} value
+ * @returns 返回 Redux 可用的 action
+ */
+const getDeviceCirculateRecordAction = (value) => ({
+  type: GET_DEVICE_CIRCULATE_RECORD,
+  value,
+});
+
+/**
+ * 通过设备 ID，获取设备流通记录
+ * @param {String} id
+ * @returns 返回 Redux 可用的 action
+ */
+export const getDeviceCirculateRecord = (id) => {
+  return (dispatch) => {
+    axios.get('/circulateRecord?deviceId=' + id).then((res) => {
+      console.log(res);
     });
   };
 };
--- frontend/src/store/actionTypes.js
+++ frontend/src/store/actionTypes.js
@@ -1,2 +1,4 @@
 export const GET_DEVICE_LIST = 'getDeviceList';
 export const GET_DEVICE_BY_NAME = 'getDeviceByName';
+export const GET_DEVICE_CARRY_RECORD = 'getDeviceCarryRecord';
+export const GET_DEVICE_CIRCULATE_RECORD = 'getDeviceCirculateRecord';
