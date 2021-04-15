6924f13f0125a794584c712a801ae43e3de733cd
syyCN <sunyiyangcn@outlook.com>
2021-04-15 14:04:41 +0800

    add buttons to staff page

--- frontend/src/pages/staff/index.js
+++ frontend/src/pages/staff/index.js
@@ -10,43 +10,6 @@ class Staff extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
-    this.state.tableHeader = [
-      {
-        title: '员工ID',
-        dataIndex: 'id',
-        key: 'id',
-      },
-      {
-        title: '姓名',
-        dataIndex: 'name',
-        key: 'name',
-      },
-      {
-        title: '性别',
-        dataIndex: 'sex',
-        key: 'sex',
-      },
-      {
-        title: '操作',
-        dataIndex: 'options',
-        key: 'options',
-        render: (text, record) => {
-          return (
-            <div className="btns">
-              <Button className="item" type="primary"></Button>
-              <Button className="item">修改设备状态</Button>
-              <Button
-                className="item"
-                danger
-                onClick={this.handleDeleteStaff(record)}
-              >
-                删除设备
-              </Button>
-            </div>
-          );
-        },
-      },
-    ];
   }
   render() {
     return (
@@ -59,7 +22,29 @@ class Staff extends Component {
           <Column title="员工 ID" dataIndex="id" key="id"></Column>
           <Column title="姓名" dataIndex="name" key="name"></Column>
           <Column title="性别" dataIndex="sex" key="sex"></Column>
-          <Column title="操作" dataIndex="options" key="options"></Column>
+          <Column
+            title="操作"
+            dataIndex="options"
+            key="options"
+            render={(text, record) => {
+              return (
+                <div className="btns">
+                  {/* 修改员工信息 */}
+                  <Button className="item" type="primary">
+                    修改员工信息
+                  </Button>
+                  {/* 删除员工 */}
+                  <Button
+                    className="item"
+                    danger
+                    onClick={this.handleDeleteStaff(record)}
+                  >
+                    删除员工
+                  </Button>
+                </div>
+              );
+            }}
+          ></Column>
         </Table>
       </div>
     );
