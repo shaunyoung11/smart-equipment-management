74cdc42b6fe81cc239b67a480a924b26daf47c73
syyCN <sunyiyangcn@outlook.com>
2021-04-21 14:25:03 +0800

    add comment to login page

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,18 +1,26 @@
+// 导入 react
 import React, { Component } from 'react';
+// 导入 antd 组件
 import { Form, Input, Button, Checkbox } from 'antd';
+// 导入 redux store
 import store from '../../store';
+// 导入样式文件
 import './style.scss';
 
 class Login extends Component {
   constructor(props) {
     super(props);
+    // 初始化 state
     this.state = store.getState();
   }
   render() {
     return (
       <div className="login">
+        {/* 登录框 */}
         <div className="box">
+          {/* 登录表单 */}
           <Form>
+            {/* 用户名 */}
             <Form.Item
               label="用户名"
               name="username"
@@ -20,6 +28,7 @@ class Login extends Component {
             >
               <Input />
             </Form.Item>
+            {/* 密码 */}
             <Form.Item
               label="密码"
               name="password"
@@ -27,9 +36,11 @@ class Login extends Component {
             >
               <Input.Password />
             </Form.Item>
+            {/* 保持登录状态 */}
             <Form.Item name="remember" valuePropName="checked">
               <Checkbox>记住我</Checkbox>
             </Form.Item>
+            {/* 登录按钮 */}
             <Form.Item>
               <Button type="primary" htmlType="submit" onClick="handleLogin">
                 登录
